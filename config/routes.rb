2cc3ea3c53095a847e707af575d11f93f65d0adc
Rabin Poudyal <rabinonrailsforruby@gmail.com>
2016-06-15 10:11:41 +0545

    Resolved the first routing problem in the cloud

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
   root 'pages#home'
-  get 'contact', to: 'pages#contact'
-  get 'about', to: 'pages#about'
+  get 'contact' => 'pages#contact'
+  get 'about' => 'pages#about'
 
   resources :articles
+
+  get 'articles' => 'articles#index'
   
 end
