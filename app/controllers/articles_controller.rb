edcc3e20b5c6a207025155d576d518b02e5ea579
Rabin Poudyal <rabinonrailsforruby@gmail.com>
2016-06-15 09:58:33 +0545

    Added all the CRUD operations for articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+	def index
+		@articles = Article.all
+	end
+
 	def new
 		@article = Article.new
 	end
@@ -6,7 +10,7 @@ class ArticlesController < ApplicationController
 		@article = Article.new(article_params)
 		if @article.save
 			flash[:notice] = "Article was sucessfully created"
-			redirect_to article_show(@article)
+			redirect_to article_path(@article)
 		else
 			render 'new'
 		end
@@ -15,6 +19,26 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 
 	end
+	def edit
+		@article = Article.find(params[:id])
+	end
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = "Article was sucessfully updated"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		flash[:notice] = "Article sucessfully deleted"
+		redirect_to articles_path
+	end
+
 	private
 		def article_params
 			params.require(:article).permit(:title,:description)
