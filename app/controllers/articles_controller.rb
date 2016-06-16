b147145895e6c0a0af3b42d81645f1e2612d8963
Rabin Poudyal <rabinonrailsforruby@gmail.com>
2016-06-16 23:16:26 +0545

    Added the pagination gem to the articles and users listing page and deciding how many stuffs should display per page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 	before_action :set_article, only: [:edit,:update,:show,:destroy]
 	def index
-		@articles = Article.all
+		@articles = Article.paginate(page: params[:page], per_page: 5)
 	end
 
 	def new
@@ -9,6 +9,7 @@ class ArticlesController < ApplicationController
 	end
 	def create
 		@article = Article.new(article_params)
+		@article.user = User.last
 		if @article.save
 			flash[:success] = "Article was sucessfully created"
 			redirect_to article_path(@article)
