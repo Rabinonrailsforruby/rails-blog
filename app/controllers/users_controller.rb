6e3dd1b270a540e6015bdfa22dc2a222cc864f4e
Rabin Poudyal <rabinonrailsforruby@gmail.com>
2016-06-17 18:17:22 +0545

    Completed the authentication and solved security issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:edit, :update, :show]
+	before_action :require_same_user, only: [:edit]
 	def index
 		@users = User.paginate(page: params[:page],per_page: 5)
 	end
@@ -18,10 +20,10 @@ class UsersController < ApplicationController
 
 	end
 	def edit
-		@user = User.find(params[:id])
+		
 	end
 	def update
-		@user = User.find(params[:id])
+		
 		if @user.update(user_params)
 			flash[:success] = "update was succesful"
 			redirect_to articles_path
@@ -30,12 +32,21 @@ class UsersController < ApplicationController
 		end
 	end
 	def show
-		@user = User.find(params[:id])
+		
 		@user_articles  = @user.articles.paginate(page: params[:page], per_page: 5)
 	end
 	
 	private
 		def user_params
 			params.require(:user).permit(:username,:email,:password)
+		end
+		def set_user
+			@user = User.find(params[:id])
+		end
+		def require_same_user
+			if current_user != @user
+				flash[:danger] = "You can only edit your own account!"
+				redirect_to root_path
+			end
 		end
 end
