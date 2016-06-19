7d91cab9518608fd16f3f98c0b4812b61049bf34
Rabin Poudyal <rabinonrailsforruby@gmail.com>
2016-06-19 23:19:57 +0545

    Added the category many to many association db model ie category

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
+
+  resources :categories, except: [:destroy]
   
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,8 +2,6 @@ require 'test_helper'
 class CategoryTest < ActiveSupport::TestCase
 	def setup
 		@category = Category.new(name: "sports")
-
-
 	end
 	test "category should be valid" do
 		assert @category.valid?
@@ -15,6 +13,14 @@ class CategoryTest < ActiveSupport::TestCase
 	test "name should be unique" do
 		@category.save
 		category2 = Category.new(name: "sports")
-		assert_not category.valid?
+		assert_not category2.valid?
+	end
+	test "name should not be too long" do
+		@category.name = 'a' * 26
+		assert_not @category.valid?
+	end
+	test "name should not be too short" do
+		@category.name = "aa"
+		assert_not @category.valid?
 	end
 end
